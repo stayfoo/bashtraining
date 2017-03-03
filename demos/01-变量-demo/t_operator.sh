5f9144941213c9997f1356b72e837dcb2b45751f
MengYP <1134471523@qq.com>
2017-03-03 09:45:18 +0800

    operator - 文件测试运算符

--- demos/01-变量-demo/t_operator.sh
+++ demos/01-变量-demo/t_operator.sh
@@ -165,3 +165,110 @@ fi
 
 # 5、 字符串运算符
 
+a="abc"
+b="efg"
+
+if [ $a = $b ]
+then
+    echo "$a = $b : a 等于 b"
+else
+    echo "$a = $b : a 不等于 b"
+fi
+
+
+if [ $a != $b ]
+then
+    echo "$a != $b : a 不等于 b"
+else
+    echo "$a != $b : a 等于 b"
+fi
+
+
+if [ -z $a ]
+then
+    echo "-z $a : 字符串长度为 0"
+else
+    echo "-z $a : 字符串长度不为 0"
+fi
+
+
+if [ -n $a ]
+then
+    echo "-n $a : 字符串长度不为 0"
+else
+    echo "-n $a : 字符串长度为 0"
+fi
+
+
+if [ $a ]
+then
+    echo "$a : 字符串不为空"
+else
+    echo "$a : 字符串为空"
+fi
+
+
+
+# 6、 文件测试运算符
+
+file="./t.sh"
+
+
+if [ -r $file ]
+then
+    echo "文件可读"
+else
+    echo "文件不可读"
+fi
+
+
+if [ -w $file ]
+then
+    echo "文件可写"
+else
+    echo "文件不可写"
+fi
+
+
+if [ -x $file ]
+then
+    echo "文件可执行"
+else
+    echo "文件不可执行"
+fi
+
+
+if [ -f $file ]
+then
+    echo "文件为普通文件"
+else
+    echo "文件为特殊文件"
+fi
+
+
+if [ -d $file ]
+then
+    echo "文件是个目录"
+else
+    echo "文件不是个目录"
+fi
+
+
+if [ -s $file ]
+then
+    echo "文件不为空"
+else
+    echo "文件为空"
+fi
+
+
+if [ -e $file ]
+then
+    echo "文件存在"
+else
+    echo "文件不存在"
+fi
+
+
+
+
